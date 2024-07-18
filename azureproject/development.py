629f5597a9fcf36ad84ad6b09495edd78a6fceea
jeter74 <62223834+jeter74@users.noreply.github.com>
2024-07-18 15:08:02 +0900

    Update development.py

--- azureproject/development.py
+++ azureproject/development.py
@@ -1,8 +1,15 @@
 import os
+SECRET_KEY=os.getenv('SECRET_KEY')
+
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']] if'WEBSITE_HOSTNAME' in os.environ else []
+
+conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-    dbuser=os.environ['DBUSER'],
-    dbpass=os.environ['DBPASS'],
-    dbhost=os.environ['DBHOST'],
-    dbname=os.environ['DBNAME']
+    dbuser=os.environ['user'],
+    dbpass=os.environ['password'],
+    dbhost=os.environ['host'],
+    dbname=os.environ['dbname']
 )
